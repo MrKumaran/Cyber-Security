4cf6f95e6df2d1ab8cfcb64f2e5800a3e076df6f
Kumaran S <kumaran.s@velammal.edu.in>
2024-10-20 01:55:24 +0530

    initial

---
+++ packetSniffer.py
@@ -0,0 +1,10 @@
+# Description: This script is a simple packet sniffer that uses scapy to capture packets from the network.
+from scapy.all import sniff
+
+def packet_analyzer(packet):
+    print(packet.summary())
+
+try:
+    sniff(prn=packet_analyzer)
+except KeyboardInterrupt:
+    print("\nSniffing stopped.")
