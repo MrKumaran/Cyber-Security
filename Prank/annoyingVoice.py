77c2eab1052ca9ff66c9375f6c4310c81d128e32
Kumaran S <kumaran.s@velammal.edu.in>
2024-11-05 23:38:05 +0530

    0.3

--- Prank/annoyingVoice.py
+++ Prank/annoyingVoice.py
@@ -3,11 +3,17 @@ import pyautogui
 import time
 
 
-def block_mouse():
+def annoyingPart():  # works on 64-bit win 11
     while True:
         pyautogui.moveTo(0, 0)  # Move mouse to top-left corner
+        engine.say("Hi, Hope you're enjoying this because I've hand crafted this for specially you hehehehehehehe! (Evil laugh)")
+        engine.runAndWait()
 
 
 if __name__ == "__main__":
     pyautogui.FAILSAFE = False
-    block_mouse()
+    print("Hi, Hope you're enjoying this because I've hand crafted this for specially you hehehehehehehe! (Evil laugh)")
+    engine = tts.init()
+    engine.say("Hi, Hope you're enjoying this because I've hand crafted this for specially you hehehehehehehe! (Evil laugh)")
+    engine.runAndWait()
+    annoyingPart()
